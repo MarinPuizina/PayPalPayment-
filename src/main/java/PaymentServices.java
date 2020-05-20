197d4c890bfc2e2aaf63df31cda0d26b856d7be7
MarinPuizina <marinpuizina@hotmail.com>
2020-05-20 19:04:28 +0200

    Add CLIENT_ID and CLIENT_SECRET to PaymentServices.java

--- src/main/java/PaymentServices.java
+++ src/main/java/PaymentServices.java
@@ -1,4 +1,8 @@
 package main.java;
 
 public class PaymentServices {
+
+    private static final String CLIENT_ID = "AQN1E3jVF39iYvxCW-tjs-tD_85hZTELmAHMstyazfl6sjdjE-mHd3NuOY725FkXanOod2-bj-VUgYxa";
+    private static final String CLIENT_SECRET = "EFesiE2H4_IJI8M--nvhzZyug8VmEaShmmKkvazp0SUrSquAdBgrOusigmHDCnXAQSd1hCGYEl6Ob1no";
+
 }
