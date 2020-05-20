c8e15999b60e1d8583b718fea45944c13219969f
MarinPuizina <marinpuizina@hotmail.com>
2020-05-20 19:05:48 +0200

    Add MODE to PaymentServices.java

--- src/main/java/PaymentServices.java
+++ src/main/java/PaymentServices.java
@@ -4,5 +4,6 @@ public class PaymentServices {
 
     private static final String CLIENT_ID = "AQN1E3jVF39iYvxCW-tjs-tD_85hZTELmAHMstyazfl6sjdjE-mHd3NuOY725FkXanOod2-bj-VUgYxa";
     private static final String CLIENT_SECRET = "EFesiE2H4_IJI8M--nvhzZyug8VmEaShmmKkvazp0SUrSquAdBgrOusigmHDCnXAQSd1hCGYEl6Ob1no";
+    private static final String MODE = "sandbox";
 
 }
