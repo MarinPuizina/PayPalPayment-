370433693b3784c1a36aa3da26358dff7b674455
MarinPuizina <marinpuizina@hotmail.com>
2020-05-25 15:32:18 +0200

    Edit refactoring OrderDetail field types to String to complement the paypalSDK

--- src/main/java/OrderDetail.java
+++ src/main/java/OrderDetail.java
@@ -3,12 +3,12 @@ package main.java;
 public class OrderDetail {
 
     private String productName;
-    private float subtotal;
-    private float shipping;
-    private float tax;
-    private float total;
+    private String subtotal;
+    private String shipping;
+    private String tax;
+    private String total;
 
-    public OrderDetail(String productName, float subtotal, float shipping, float tax, float total) {
+    public OrderDetail(String productName, String subtotal, String shipping, String tax, String total) {
         this.productName = productName;
         this.subtotal = subtotal;
         this.shipping = shipping;
@@ -21,40 +21,20 @@ public class OrderDetail {
         return productName;
     }
 
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
     public String getSubtotal() {
         return String.format("%.2f", subtotal);
-    }
-
-    public void setSubtotal(float subtotal) {
-        this.subtotal = subtotal;
     }
 
     public String getShipping() {
         return String.format("%.2f", shipping);
     }
 
-    public void setShipping(float shipping) {
-        this.shipping = shipping;
-    }
-
     public String getTax() {
         return String.format("%.2f", tax);
     }
 
-    public void setTax(float tax) {
-        this.tax = tax;
-    }
-
     public String getTotal() {
         return String.format("%.2f", total);
-    }
-
-    public void setTotal(float total) {
-        this.total = total;
     }
 
 }
