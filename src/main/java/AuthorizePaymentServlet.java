3ff165f384afda20423e4cab3f87b0c4f3f3b29d
MarinPuizina <marinpuizina@hotmail.com>
2020-05-25 15:36:34 +0200

    Edit doPost servlet method, get approvalLink and do redirect

--- src/main/java/AuthorizePaymentServlet.java
+++ src/main/java/AuthorizePaymentServlet.java
@@ -1,5 +1,7 @@
 package main.java;
 
+import com.paypal.base.rest.PayPalRESTException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,9 +12,26 @@ import java.io.IOException;
 @WebServlet(name = "authorize_payment") // mush match the form action (checkout.html)
 public class AuthorizePaymentServlet extends HttpServlet {
 
-
-
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // Getting parameters from a form
+        String product = request.getParameter("product");
+        String subtotal = request.getParameter("subtotal");
+        String shipping = request.getParameter("shipping");
+        String tax = request.getParameter("tax");
+        String total = request.getParameter("total");
+
+        OrderDetail orderDetail = new OrderDetail(product, subtotal, shipping, tax, total);
+
+        PaymentServices paymentServices = new PaymentServices();
+        String approvalLink = null;
+        try {
+            approvalLink = paymentServices.authorizePayment(orderDetail);
+        } catch (PayPalRESTException e) {
+            e.printStackTrace();
+        }
+
+        response.sendRedirect(approvalLink);
 
     }
 
